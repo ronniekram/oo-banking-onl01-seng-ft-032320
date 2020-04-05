7e025780c2cf1c94cab3d394a2f5aa16024c3a86
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-05 18:15:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,9 +21,6 @@ class Transfer
         receiver.deposit(amount)
         self.status = "complete"
       end
-    elsif sender.balance < amount
-      self.status = "rejected"
-      return "Transaction rejected. Please check your account balance."
     else
       self.status = "rejected"
       return "Transaction rejected. Please check your account balance."
